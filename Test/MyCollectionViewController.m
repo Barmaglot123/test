e8dd4d4c2944d5a1fe3a04490ca119ded55b1b10
Barmaglot213 <denislyam.i.i14@gmail.com>
2017-04-09 20:37:03 +0300

    09.04.17 NSFetchedResulltsController Delete and Insert

--- Test/MyCollectionViewController.m
+++ Test/MyCollectionViewController.m
@@ -11,14 +11,14 @@
 #import "MyCell.h"
 #import "AppDelegate.h"
 #import "Test+CoreDataModel.h"
-//#import <Realm/Realm.h>
 
 
-@interface MyCollectionViewController ()
+@interface MyCollectionViewController () <NSFetchedResultsControllerDelegate>
 @property (strong, nonatomic) NSMutableArray * reloadSongs;
 @property (strong, nonatomic) NSMutableArray * songs;
 @property (strong, nonatomic) UIRefreshControl *refreshControl;
 @property (strong, nonatomic) NSManagedObjectContext *context;
+@property (strong, nonatomic) NSFetchedResultsController * fetchedResultController;
 @end
 
 @implementation MyCollectionViewController
@@ -29,8 +29,19 @@ static NSString * const reuseIdentifier = @"Cell";
     [super viewDidLoad];
     self.context = ((AppDelegate*)[[UIApplication sharedApplication] delegate]).persistentContainer.viewContext;
     NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] initWithEntityName:@"SongsEntity"];
-    fetchRequest.resultType = NSDictionaryResultType;
-    self.songs = [[self.context executeFetchRequest:fetchRequest error:nil] mutableCopy];
+    //fetchRequest.resultType = NSDictionaryResultType;
+    NSSortDescriptor *idSortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"id" ascending:YES];
+    fetchRequest.sortDescriptors = @[idSortDescriptor];
+    
+    self.fetchedResultController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:self.context sectionNameKeyPath:nil cacheName:nil];
+    [[self fetchedResultController] setDelegate:self];
+    NSError *error = nil;
+    if (![self.fetchedResultController performFetch:&error]) {
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+    
+    //self.songs = [[self.context executeFetchRequest:fetchRequest error:nil] mutableCopy];
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.collectionView addSubview:self.refreshControl];
     [self.refreshControl addTarget:self action:@selector(loadData) forControlEvents:UIControlEventValueChanged];
@@ -49,9 +60,10 @@ static NSString * const reuseIdentifier = @"Cell";
     return 1;
 }
 
-
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.songs.count;
+   // return self.songs.count;
+    id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchedResultController sections][section];
+    return [sectionInfo numberOfObjects];
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
@@ -60,13 +72,12 @@ static NSString * const reuseIdentifier = @"Cell";
     cell.layer.borderWidth=1.0f;
     cell.layer.borderColor=[UIColor blackColor].CGColor;
 
-    cell.artistNameLabel.text = [[self.songs objectAtIndex:indexPath.row] objectForKey:@"author"];
-    cell.songNameLabel.text = [[self.songs objectAtIndex:indexPath.row]objectForKey:@"label"];
+    cell.artistNameLabel.text = [[self.fetchedResultController objectAtIndexPath:indexPath]valueForKey:@"author"];
+    cell.songNameLabel.text = [[self.fetchedResultController objectAtIndexPath:indexPath]valueForKey:@"label"];
     
     return cell;
 }
 
-#pragma mark <UICollectionViewDelegate>
 #pragma mark Load Data
 -(void)loadData{
     
@@ -81,52 +92,31 @@ static NSString * const reuseIdentifier = @"Cell";
         for (int i = 0; i < [responseObject count]; i++ ){
         
             if(![tempArray containsObject:[responseObject objectAtIndex:i]]){
-            
-                
                 NSManagedObject * newSong = [NSEntityDescription insertNewObjectForEntityForName:@"SongsEntity" inManagedObjectContext: self.context];
-                
                 [newSong setValue:[[responseObject objectAtIndex:i] objectForKey:@"label"] forKey:@"label"];
                 [newSong setValue:[[responseObject objectAtIndex:i] objectForKey:@"author"] forKey:@"author"];
                 [newSong setValue:[[responseObject objectAtIndex:i] objectForKey:@"id"] forKey:@"id"];
                 [newSong setValue:[[responseObject objectAtIndex:i] objectForKey:@"version"] forKey:@"version"];
+                
+                [self saveContext];
+
                 NSLog(@"%@",[responseObject objectAtIndex:i]);
                 
-                
-                [self.songs insertObject:[responseObject objectAtIndex:i] atIndex:0];
-                
-                
-                NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
-                [self.collectionView insertItemsAtIndexPaths:@[indexPath]];
+
             }
             if(i < tempArray.count){
                 if(![responseObject containsObject:[tempArray objectAtIndex:i]]){
-                    
-                    NSPredicate *predicateID = [NSPredicate predicateWithFormat:@"id == %d",[[tempArray objectAtIndex:i]objectForKey:@"id"]];
-                    
-                    NSManagedObjectContext *myContext = self.context;
-                    NSFetchRequest *fetchToDeleteRequest = [[NSFetchRequest alloc] init];
-                    [fetchToDeleteRequest setEntity:[NSEntityDescription entityForName:@"SongsEntity" inManagedObjectContext:myContext]];
-                    [fetchToDeleteRequest setIncludesPropertyValues:NO];
-                    [fetchToDeleteRequest setPredicate:predicateID];
+                               
+                    NSManagedObject *object = [self.fetchedResultController objectAtIndexPath:[NSIndexPath indexPathForItem:i inSection:0]];
+                    NSManagedObjectContext *myContext = object.managedObjectContext;
+                    [myContext deleteObject:object];
 
-                    NSError *error = nil;
-                    NSArray *deleteObject = [myContext executeFetchRequest:fetchToDeleteRequest error:&error];
-                    
-                    for (NSManagedObject *object in deleteObject) {
-                        [myContext deleteObject:object];
-                    }
-                    
-                    
-                    [self.songs removeObject:[tempArray objectAtIndex:i]];
-                    
-                    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:i inSection:0];
-                    [self.collectionView deleteItemsAtIndexPaths:@[indexPath]];
+                    [self saveContext];
                 }
             }
-            
         }
+        [self.refreshControl endRefreshing];
 
-       NSLog(@"JSON: %@", responseObject);
     } failure:^(NSURLSessionTask *operation, NSError *error) {
         NSLog(@"Error: %@", error);
     }];
@@ -152,5 +142,42 @@ static NSString * const reuseIdentifier = @"Cell";
     
     
 }
+- (void)saveContext {
+    NSManagedObjectContext *context = self.context;
+    
+    NSError *error = nil;
+    if (![context save:&error]) {
+        NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        abort();
+    }
+}
+
+#pragma mark NSFetchedResultsController
+- (void)controller:(NSFetchedResultsController *)controller
+   didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)indexPath
+     forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath {
+    //UITableView *tableView = self.tableView;
+    UICollectionView * collectionView = self.collectionView;
+    
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [collectionView insertItemsAtIndexPaths:@[newIndexPath] ];
+            break;
+            
+        case NSFetchedResultsChangeDelete:
+            [collectionView deleteItemsAtIndexPaths:@[indexPath]];
+            break;
+            
+        case NSFetchedResultsChangeMove:
+            break;
+            
+        case NSFetchedResultsChangeUpdate:
+            break;
+
+    }
+}
+
 
 @end
